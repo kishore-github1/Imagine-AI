75791edd243f4b749e52ca02503d910223f1b10f
kishore <kishore.d21@iiits.in>
2024-07-23 19:21:50 +0530

    update backend urls

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8000/api/v1/post', {
+        const response = await fetch('https://imagine-ai-server.vercel.app/api/v1/post', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
