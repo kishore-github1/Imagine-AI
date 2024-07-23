de4778ab4e399a9b5dec8d75f80e410ade77bf37
kishore <kishore.d21@iiits.in>
2024-07-23 17:20:56 +0530

    Update cors

--- server/index.js
+++ server/index.js
@@ -9,11 +9,7 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors({
-    origin : [""],
-    methods : ["POST","GET"],
-    credentials:true
-}));
+app.use(cors());
 app.use(express.json({limit:'50mb'}));
 
 app.use('/api/v1/post', postRoutes);
